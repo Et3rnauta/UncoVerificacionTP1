66bc85ab77f3a80e718e941161eadb0d2df09033
guido <guido@192.168.0.175>
2020-04-27 16:48:14 -0300

    v1.0

--- src/CruzarPadres.java
+++ src/CruzarPadres.java
@@ -8,66 +8,81 @@ public class CruzarPadres {
 
     }
 
-    private static void cruzarPadres(boolean[] padre, boolean[] madre) {
-        boolean[] geno1 = new boolean[N], geno2 = new boolean[N];
-        for (int i = 0; i < N; i++) {
-            geno1[i] = padre.genotipo[i];
-            geno2[i] = padre.genotipo[i];
+    private static void testAlgoritmo(boolean[] padre, boolean[] madre) {
+        System.out.print("padre");
+        printArray(padre);
+        System.out.print("madre");
+        printArray(madre);
+
+        ArrayList<boolean[]> resultado = cruzarPadres(padre, madre);
+
+        System.out.print("hijo1");
+        printArray(resultado.get(0));
+        System.out.println("hijo2");
+        printArray(resultado.get(1));
+    }
+
+    private static ArrayList<boolean[]> cruzarPadres(boolean[] padre, boolean[] madre) {
+        boolean[] hijo1 = new boolean[padre.length], hijo2 = new boolean[padre.length];
+        for (int i = 0; i < padre.length; i++) {
+            hijo1[i] = padre[i];
+            hijo2[i] = padre[i];
         }
-        Ind hijo1 = new Ind(geno1),
-                hijo2 = new Ind(geno2);
 
         boolean alternador = true;
         int balance = 0;
-        for (int i = 0; i < N; i++) {
-            if (padre.genotipo[i] != madre.genotipo[i]) {
+        for (int i = 0; i < padre.length; i++) {
+            if (padre[i] != madre[i]) {
                 /* Si el valor es distinto, le asigna al primer hijo el mismo valor que el padre y el opuesto al 
                 segundo. Luego, en la proxima diferencia, asigna los valores opuestos para balancear el arreglo
                 y cambia el alternador, para que (si existe algun otra diferencia) realice el mismo proceso pero 
                 esta vez, mirando al segundo hijo*/
                 if (alternador) {
                     if (balance == 0) {
-                        hijo2.genotipo[i] = !padre.genotipo[i];
-                        if (padre.genotipo[i]) {
+                        hijo2[i] = !padre[i];
+                        if (padre[i]) {
                             balance++;
                         } else {
                             balance--;
                         }
                     } else {
                         if (balance > 0) {
-                            hijo1.genotipo[i] = false;
-                            hijo2.genotipo[i] = true;
+                            hijo1[i] = false;
+                            hijo2[i] = true;
                             balance--;
                         } else {
-                            hijo1.genotipo[i] = true;
-                            hijo2.genotipo[i] = false;
+                            hijo1[i] = true;
+                            hijo2[i] = false;
                             balance++;
                         }
                         alternador = !alternador;
                     }
                 } else if (balance == 0) {
-                    hijo1.genotipo[i] = !padre.genotipo[i];
-                    if (padre.genotipo[i]) {
+                    hijo1[i] = !padre[i];
+                    if (padre[i]) {
                         balance++;
                     } else {
                         balance--;
                     }
                 } else {
                     if (balance > 0) {
-                        hijo2.genotipo[i] = false;
-                        hijo1.genotipo[i] = true;
+                        hijo2[i] = false;
+                        hijo1[i] = true;
                         balance--;
                     } else {
-                        hijo2.genotipo[i] = true;
-                        hijo1.genotipo[i] = false;
+                        hijo2[i] = true;
+                        hijo1[i] = false;
                         balance++;
                     }
                     alternador = !alternador;
                 }
             }
         }
-        hijosN.add(hijo1);
-        hijosN.add(hijo2);
+
+        ArrayList<boolean[]> lista = new ArrayList<>();
+        lista.add(hijo1);
+        lista.add(hijo2);
+        return lista;
     }
 
     public static void printArray(boolean[] array) {
