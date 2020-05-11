fc35f46f21f55857e0e416ce7d36ebdd7524e9b9
guido <guido@192.168.0.175>
2020-05-11 18:22:02 -0300

    Creacion de JUnit

--- src/main/CruzarPadres.java
+++ src/main/CruzarPadres.java
@@ -1,46 +1,10 @@
+package main;
 
 import java.util.ArrayList;
 
 // @author guido
 public class CruzarPadres {
-
-    public static void main(String[] args) {
-        int[] padre, madre;
-
-        //Primer caso de test
-        padre = new int[]{};
-        madre = new int[]{};
-        testAlgoritmo(padre, madre);
-        
-        //Segundo caso de test
-        padre = new int[]{1, 1, 1, 0, 0, 0};
-        madre = new int[]{1, 1, 1, 0, 0, 0};
-        testAlgoritmo(padre, madre);
-        
-        
-        //Tercer caso de test
-        padre = new int[]{1, 0, 1, 0, 1, 0};
-        madre = new int[]{0, 1, 0, 1, 0, 1};
-        testAlgoritmo(padre, madre);
-    }
-
-    private static void testAlgoritmo(int[] padre, int[] madre) {
-        boolean[] padreBool = transformar(padre),
-                madreBool = transformar(madre);
-        System.out.print("padre");
-        printArray(padreBool);
-        System.out.print("madre");
-        printArray(madreBool);
-
-        ArrayList<boolean[]> resultado = cruzarPadres(padreBool, madreBool);
-
-        System.out.print("hijo1");
-        printArray(resultado.get(0));
-        System.out.print("hijo2");
-        printArray(resultado.get(1));
-    }
-
-    private static ArrayList<boolean[]> cruzarPadres(boolean[] padre, boolean[] madre) {
+    public static ArrayList<boolean[]> cruzarPadres(boolean[] padre, boolean[] madre) {
         boolean[] hijo1 = new boolean[padre.length], hijo2 = new boolean[padre.length];
         int contador;
 
@@ -108,27 +72,5 @@ public class CruzarPadres {
         lista.add(hijo1);
         lista.add(hijo2);
         return lista;
-    }
-
-    public static void printArray(boolean[] array) {
-        System.out.print("{ ");
-        for (int contador = 0; contador < array.length; contador++) {
-            if (array[contador]) {
-                System.out.print("1 ");
-            } else {
-                System.out.print("0 ");
-            }
-        }
-        System.out.println(" }");
-    }
-
-    private static boolean[] transformar(int[] array) {
-        boolean[] arrayBool = new boolean[array.length];
-
-        for (int contador = 0; contador < arrayBool.length; contador++) {
-            arrayBool[contador] = array[contador] > 0;
-        }
-
-        return arrayBool;
     }
 }
---
+++ test/main/CruzarPadresTest.java
@@ -0,0 +1,84 @@
+package main;
+
+import java.util.ArrayList;
+import org.junit.Test;
+import main.CruzarPadres;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author guido
+ */
+public class CruzarPadresTest {
+    @Test
+    public void testCruzarPadres1() {
+        System.out.println("*Se ejecuta el primer test del metodo cruzarPadres*");
+        int[] padre = {1, 0, 1, 0},
+                madre = {0, 1, 0, 1};
+
+        ArrayList<boolean[]> hijos = CruzarPadres.cruzarPadres(transformar(padre), transformar(madre));
+
+        boolean[] hijo1, hijo2;
+
+        assertEquals(2, hijos.size());//Tiene solo 2 hijos
+
+        hijo1 = hijos.get(0);
+        hijo2 = hijos.get(1);
+
+        assertEquals(padre.length, hijo1.length);//La longitud del primer hijo es igual al padre
+        assertEquals(padre.length, hijo2.length);//La longitud del segundo hijo es igual al padre
+
+        //Tienen cantidad igual o aproximada de cantidad de valores booleanos
+        assertTrue(estaBalanceado(hijo1));
+        assertTrue(estaBalanceado(hijo2));
+    }
+    
+    @Test
+    public void testCruzarPadres2() {
+        System.out.println("*Se ejecuta el segundo test del metodo cruzarPadres*");
+        int[] padre = {1, 0, 1, 0},
+                madre = {0, 1, 0, 1};
+
+        ArrayList<boolean[]> hijos = CruzarPadres.cruzarPadres(transformar(padre), transformar(madre));
+
+        boolean[] hijo1, hijo2;
+
+        assertEquals(2, hijos.size());//Tiene solo 2 hijos
+
+        hijo1 = hijos.get(0);
+        hijo2 = hijos.get(1);
+
+        assertEquals(padre.length, hijo1.length);//La longitud del primer hijo es igual al padre
+        assertEquals(padre.length, hijo2.length);//La longitud del segundo hijo es igual al padre
+
+        //Tienen cantidad igual o aproximada de cantidad de valores booleanos
+        assertTrue(estaBalanceado(hijo1));
+        assertTrue(estaBalanceado(hijo2));
+    }
+    
+    private static boolean[] transformar(int[] array) {
+        //Recibe un arreglo con 1 y 0, y lo transforma a booleanos [true = 1, false = 0]
+        boolean[] arrayBool = new boolean[array.length];
+
+        for (int contador = 0; contador < arrayBool.length; contador++) {
+            arrayBool[contador] = array[contador] > 0;
+        }
+
+        return arrayBool;
+    }
+
+    private static boolean estaBalanceado(boolean[] arrBoolean) {
+        //Busca que el arreglo tenga igual numero de valores booleanos
+        //Permite que como máximo sea 1, en los casos donde tiene longitud impar
+        int contTrue = 0, contFalse = 0, res;
+        for (boolean valBool : arrBoolean) {
+            if (valBool) {
+                contTrue++;
+            } else {
+                contFalse++;
+            }
+        }
+        res = Math.abs(contTrue - contFalse);
+        return res == 0 || res == 1;
+    }
+}
